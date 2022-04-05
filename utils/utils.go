31451219a56c8d1e1c45575a17a388101540c256
Tadhg Codyre <tadhgcodyre@protonmail.com>
2022-04-05 16:21:01 +0100

    updated utils

--- utils/utils.go
+++ utils/utils.go
@@ -1,29 +1,7 @@
 package utils
 
-import (
-	"log"
-	"strconv"
-)
-
 func CheckErr(err error) {
 	if err != nil {
 
-	}
-}
-
-func ConvertStringToInt(array string) int {
-	newString, err := strconv.Atoi(array)
-	if err != nil {
-		log.Fatal("Error converting string to int: ", err)
-	}
-
-	return newString
-}
-
-func CheckboxResponse(response string) bool {
-	if response == "true" {
-		return true
-	} else {
-		return false
 	}
 }
