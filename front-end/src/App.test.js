d47ece3e9c56b2d8b0da66e5af9bcb8bad0d5e3c
Tadhg Codyre <tadhgcodyre@protonmail.com>
2022-05-07 13:59:14 +0100

    Added front end testing

--- front-end/src/App.test.js
+++ front-end/src/App.test.js
@@ -1,8 +1,65 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
+import PoolQuiz from "./web app/Pool-Quiz";
+import {mount, shallow} from "enzyme";
+import Questions from "./web app/Questions";
+import Quiz from "./web app/Quiz";
+import QuizSetup from "./web app/QuizSetup";
+import SignIn from "./web app/SignIn";
+import SignUp from "./web app/SignUp";
+import Leaderboard from "./web app/Leaderboard";
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+describe("<App />", () => {
+  it("App renders without crashing", () => {
+    shallow(<App />);
+  });
+});
+
+// Tests for PoolQuiz
+describe("<PoolQuiz />", () => {
+  it("Homepage renders without crashing", () => {
+    shallow(<PoolQuiz />);
+  });
+});
+
+// Tests for Questions
+describe("<Questions />", () => {
+  it("Questions renders without crashing", () => {
+    shallow(<Questions />);
+  });
+});
+
+// Tests for Quiz
+describe("<Quiz />", () => {
+  it("Quiz renders without crashing", () => {
+    shallow(<Quiz />);
+  });
+});
+
+// Tests for QuizSetup
+describe("<QuizSetup />", () => {
+  it("QuizSetup renders without crashing", () => {
+    shallow(<QuizSetup />);
+  });
+});
+
+// Tests for SignIn
+describe("<SignIn />", () => {
+  it("SignIn renders without crashing", () => {
+    shallow(<SignIn />);
+  });
+});
+
+// Tests for SignUp
+describe("<SignUp />", () => {
+  it("SignUp renders without crashing", () => {
+    shallow(<SignUp />);
+  });
+});
+
+// Tests for Leaderboard
+describe("<Leaderboard />", () => {
+  it("SignUp renders without crashing", () => {
+    shallow(<Leaderboard />);
+  });
 });
